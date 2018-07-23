421aada15c8532a5004682b5e8dbe6ac86e7b50c
juanOblitas <joblitas17@jaumebalmes.net>
2018-07-23 07:06:32 +0200

    modify link bootstrap in dishesSelects.php

--- Presentacion/dishesSelects.php
+++ Presentacion/dishesSelects.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Menu</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
 </head>
 <body>
     <?php
